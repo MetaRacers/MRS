5f2e1a008d8c6e445de26886a59b19a0102d23f8
David Hoang <itmanvn07@gmail.com>
2021-12-08 10:39:27 +0700

    Update MRS.sol

--- contracts/MRS.sol
+++ contracts/MRS.sol
@@ -469,42 +469,6 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     ) internal virtual {}
 }
 
-/**
- * @dev Extension of {ERC20} that allows token holders to destroy both their own
- * tokens and those that they have an allowance for, in a way that can be
- * recognized off-chain (via event analysis).
- */
-abstract contract ERC20Burnable is Context, ERC20 {
-    /**
-     * @dev Destroys `amount` tokens from the caller.
-     *
-     * See {ERC20-_burn}.
-     */
-    function burn(uint256 amount) public virtual {
-        _burn(_msgSender(), amount);
-    }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
-     * allowance.
-     *
-     * See {ERC20-_burn} and {ERC20-allowance}.
-     *
-     * Requirements:
-     *
-     * - the caller must have allowance for ``accounts``'s tokens of at least
-     * `amount`.
-     */
-    function burnFrom(address account, uint256 amount) public virtual {
-        uint256 currentAllowance = allowance(account, _msgSender());
-        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
-        unchecked {
-            _approve(account, _msgSender(), currentAllowance - amount);
-        }
-        _burn(account, amount);
-    }
-}
-
 
 /**
  * @dev Contract module which provides a basic access control mechanism, where
@@ -1007,7 +971,7 @@ library SafeMath {
     }
 }
 
-contract MRS is ERC20, Ownable, ERC20Burnable {
+contract MRS is ERC20, Ownable {
     using SafeMath for uint256;
     using Address for address;
 
